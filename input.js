3cd2d2b9ef33fc3670fec989b2fa704ae457a6d0
Hamsa Sarees <hamsasarees@gmail.com>
2026-02-01 04:39:40 +0530

    Update input.js

--- input.js
+++ input.js
@@ -1,1 +1,11 @@
+const TIGEN_Input = {
+  keys:{},
+
+  init(){
+    window.addEventListener("keydown",e=>this.keys[e.code]=true);
+    window.addEventListener("keyup",e=>this.keys[e.code]=false);
+  }
+};
+
+TIGEN_Input.init();
 
