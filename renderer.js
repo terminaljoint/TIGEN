f5f5fa21b25f8809adebcff94f49b4eb987997c6
Hamsa Sarees <hamsasarees@gmail.com>
2026-02-01 04:29:43 +0530

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,1 +1,17 @@
+class Renderer {
+  constructor(scene, vp){
+    this.camera = new THREE.PerspectiveCamera(70, vp.clientWidth/vp.clientHeight, 0.1, 2000);
+    this.camera.position.set(20,20,20);
+
+    this.renderer = new THREE.WebGLRenderer({antialias:true});
+    this.renderer.setSize(vp.clientWidth, vp.clientHeight);
+    vp.appendChild(this.renderer.domElement);
+
+    this.scene = scene;
+  }
+
+  render(){
+    this.renderer.render(this.scene, this.camera);
+  }
+}
 
